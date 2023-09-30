55bb8850d5ba6f12d9019c7807637f5d458fefc2
Febriansyah <fbriansyah@gmail.com>
2023-09-30 16:28:18 +0700

    add grpc client and server config

--- util/config.go
+++ util/config.go
@@ -7,11 +7,13 @@ import (
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variable.
 type Config struct {
-	Environment       string `mapstructure:"ENVIRONMENT"`
-	DBDriver          string `mapstructure:"DB_DRIVER"`
-	DBSource          string `mapstructure:"DB_SOURCE"`
-	MigrationURL      string `mapstructure:"MIGRATION_URL"`
-	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
+	Environment          string `mapstructure:"ENVIRONMENT"`
+	DBDriver             string `mapstructure:"DB_DRIVER"`
+	DBSource             string `mapstructure:"DB_SOURCE"`
+	MigrationURL         string `mapstructure:"MIGRATION_URL"`
+	HTTPServerAddress    string `mapstructure:"HTTP_SERVER_ADDRESS"`
+	GrpcPort             int    `mapstructure:"GRPC_PORT"`
+	SessionServerAddress string `mapstructure:"SESSION_SERVER_ADDRESS"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
