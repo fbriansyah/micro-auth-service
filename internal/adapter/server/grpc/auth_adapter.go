cb218200a61c3fab62dc95655f3a2076ebee4138
Febriansyah <fbriansyah@gmail.com>
2023-10-01 12:43:38 +0700

    implement login

--- internal/adapter/server/grpc/auth_adapter.go
+++ internal/adapter/server/grpc/auth_adapter.go
@@ -2,17 +2,33 @@ package grpcserver
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/fbriansyah/micro-payment-proto/protogen/go/auth"
 	"github.com/fbriansyah/micro-payment-proto/protogen/go/session"
+	"google.golang.org/grpc/codes"
 )
 
-func (a *GrpcServerAdapter) Login(context.Context, *auth.LoginRequest) (*auth.LoginResponse, error) {
+func (a *GrpcServerAdapter) Login(ctx context.Context, req *auth.LoginRequest) (*auth.LoginResponse, error) {
+	user, err := a.service.Login(ctx, req.Username, req.Password)
+	if err != nil {
+		return nil, generateError(
+			codes.FailedPrecondition,
+			fmt.Sprintf("error login: %v", err),
+		)
+	}
 
 	return &auth.LoginResponse{
-		Userid:  "",
-		Name:    "",
-		Session: &session.Session{},
+		Userid: user.ID.String(),
+		Name:   user.Fullname,
+		Session: &session.Session{
+			Id:                    user.Session.Id,
+			UserId:                user.ID.String(),
+			AccessToken:           user.Session.AccessToken,
+			RefreshToken:          user.Session.RefreshToken,
+			AccessTokenExpiresAt:  user.Session.AccessTokenExpiresAt,
+			RefreshTokenExpiresAt: user.Session.RefreshTokenExpiresAt,
+		},
 	}, nil
 }
 
