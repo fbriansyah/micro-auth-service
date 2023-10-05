9e68fe25246ba5eede96a8cf7db630cc662c6dc4
Febriansyah <fbriansyah@gmail.com>
2023-10-05 19:28:09 +0700

    add users id to dummy data

--- db/migration/000003_init-data.up.sql
+++ db/migration/000003_init-data.up.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (username, password, fullname)
-VALUES ('test', '$2a$10$e.Ove5P8HBOEE0udC4/XAO41wBaAESOYLWryhobm6mjFWH.YevDau', 'Test User');
+INSERT INTO users (id, username, password, fullname)
+VALUES ('ad61a78b-0f66-44d6-a579-695d8fec883f','test', '$2a$10$e.Ove5P8HBOEE0udC4/XAO41wBaAESOYLWryhobm6mjFWH.YevDau', 'Test User');
